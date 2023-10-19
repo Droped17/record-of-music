5cc9f18faa0da63c2ad254247a15058426cb25cb
Droped17 <droped17@gmail.com>
2023-10-19 23:46:22 +0700

    update add to cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,16 +1,30 @@
-export default function CartItem() {
+import { useEffect,useState } from "react";
+import axios from "../config/axios";
+
+export default function CartItem({id,amount}) {
+  const [productById, setProductById] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:1112/product/${id}`)
+      .then((res) => setProductById(res.data.product))
+      .catch((e) => console.log(e));
+  }, []);
+
+  // console.log(productById);
+
   return (
     <>
       <div className="bg-gray-200 p-2 flex gap-5 items-center justify-between">
         <div className="flex gap-5">
           <img
             className="h-20 w-20 object-cover"
-            src="https://images.unsplash.com/photo-1616681255209-368a2cd3e643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80"
+            src={productById.image}
             alt="homepage"
           />
           <div className="flex flex-col justify-center">
-            <div>album_name</div>
-            <div>price: 99999</div>
+            <div>ALBUM NAME: {productById.albumName}</div>
+            <div>{amount}</div>
+            <div>PRICE: {productById.price}</div>
           </div>
         </div>
       </div>
